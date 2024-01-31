55fccfa5bcc8a9d6739ebec9f666cbda0db88de2
johnnathanieI <117893352+johnnathanieI@users.noreply.github.com>
2024-01-31 04:23:33 -0700

    Did a whole lot of clean up and getting used to.
    - Replace App.tsx with App.jsx
    - Put App.jsx in main directory instead
    - Started on Schedule.jsx
    - Will get Schedule done soon

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    Stylesheet,
+    Text,
+    View
+} from 'react-native';
+import Schedule from './src/screens/Schedule';
+
+const App = () => {
+    return (
+        <Schedule/>
+    );
+};
+
+export default App;
---
+++ src/screens/Schedule.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+    Stylesheet,
+    Text,
+    View
+} from 'react-native';
+
+const Schedule = () => {
+    return (
+        <View>
+            <Text>Hello schedule!</Text>
+        </View>
+    );
+}
+
+export default Schedule;
