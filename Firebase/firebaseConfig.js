e86e361fce63409f953ed6acb02187648c0dc112
Carter <cdsarney@gmail.com>
2024-01-31 23:30:29 -0700

    Data transferred?
    Data can transfer from firestore onto local machine needs fine tune
    
    Co-Authored-By: MarcusVuorinen <100794285+MarcusVuorinen@users.noreply.github.com>

--- Firebase/firebaseConfig.js
+++ Firebase/firebaseConfig.js
@@ -1,6 +1,6 @@
 import {initializeApp} from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBz-1JYWPWsPfTkbdenKi_n_Cmbvn7uBqU",
@@ -13,5 +13,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const authentication = getAuth(app);
+//export const authentication = getAuth(app);
 
