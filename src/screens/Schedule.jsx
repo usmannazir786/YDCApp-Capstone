bf3d739e089edf7fd9adf7ff7ba7c5cdc754b0fe
johnnathanieI <117893352+johnnathanieI@users.noreply.github.com>
2024-02-07 04:44:44 -0700

    Database and User register coding
    -- Found how to use database and store
        data
    -- Added a couple of libraries that help a
        lot for the functions I want to work

--- src/screens/Schedule.jsx
+++ src/screens/Schedule.jsx
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
     StyleSheet,
     Text,
     TouchableOpacity,
-    View
+    View,
+    SafeAreaView,
+    Modal,
 } from 'react-native';
 /*
     Using a library that created agendas already from: https://github.com/wix/react-native-calendars
+    https://www.youtube.com/watch?v=RdaQIkE47Og&t=803s&ab_channel=JonasGr%C3%B8ndahl -- Agenda guide
 */
 import { Agenda } from 'react-native-calendars';
 /*
@@ -17,11 +20,26 @@ import { Card } from 'react-native-paper';
     Using moment to format dates
 */
 import moment from 'moment';
-//https://www.youtube.com/watch?v=RdaQIkE47Og&t=803s&ab_channel=JonasGr%C3%B8ndahl -- Agenda guide
+/*
+    Imports for the database end
+*/
+import { db } from '../../Firebase/firebaseConfig'; 
+import { collection, addDoc } from 'firebase/firestore/lite';
+/* 
+    Imports for a library that handles checking for outside clicking: https://github.com/jakex7/react-native-click-outside --SO NAYCE--
+*/
+import { ClickOutsideProvider, useClickOutside } from 'react-native-click-outside';
+//Video reference for button smooth: https://www.youtube.com/watch?v=HfZ7pdhS43s&ab_channel=TK
 
 const Schedule = () => {
     const [items, setItems] = useState([]);
     const [currDate, setCurrDate] = useState(new Date());
+    //States for schedule button
+    const [registerModalStatus, setRegisterModalStatus] = useState(false);
+
+    const screenRef = useClickOutside<SafeAreaView>(() => {
+
+    });
 
 //Creating a new function that keeps checking for the current date
     useEffect(() => {
@@ -51,17 +69,18 @@ const formatDate = moment(currDate).format('YYYY-MM-DD');
 //Creates the days and renders them into an array to be used by Agenda
     const loadItems = (day) => {
         const items = items || {};
-    
+        
+        //Creates random items from 15 before and 85 days after the selected date
         setTimeout(() => {
           for (let i = -15; i < 85; i++) {
             const time = day.timestamp + i * 24 * 60 * 60 * 1000;
             const strTime = timeToString(time);
-    
+            
+            //If it doesn't exist already, initialize strTime property to an array
             if (!items[strTime]) {
               items[strTime] = [];
               
-              //Creates item to be placed in the schedule
-              // will be replaced with the ability for the admin to create stuff here
+              // Fills the contents of the array with random variables
               const numItems = Math.floor(Math.random() * 3 + 1);
               for (let j = 0; j < numItems; j++) {
                 items[strTime].push({
@@ -82,10 +101,18 @@ const formatDate = moment(currDate).format('YYYY-MM-DD');
         }, 1000);
       };
 
+//Tester function to check if it sends into the database will use this for when the user actually registers themselves in
+      const test = async () => {
+        const itemsRef = collection(db, 'schedule');
+        const docRef = await addDoc(itemsRef, { name: "works" });
+      }
+
 //Renders the card in for the renderItem option
       const renderItem = (items) => {
 
-        return (<TouchableOpacity>
+        return (<TouchableOpacity onPress={() => {
+            setRegisterModalStatus(!registerModalStatus)
+        }}>
             <Card>
                 <Card.Content>
                     <View style={styles.card}>
@@ -94,16 +121,32 @@ const formatDate = moment(currDate).format('YYYY-MM-DD');
                 </Card.Content>
             </Card>
         </TouchableOpacity>)
-
       }
 
     return (
-        <Agenda
-        items={items}
-        loadItemsForMonth={loadItems}
-        selected={formatDate}
-        renderItem={renderItem}
-        />
+        <ClickOutsideProvider>
+            <SafeAreaView style={styles.container} useRef={screenRef}>
+                <Agenda
+                items={items}
+                loadItemsForMonth={loadItems}
+                selected={formatDate}
+                renderItem={renderItem}
+                />
+                <Modal visible={registerModalStatus} animationType='slider'>
+                    <View>
+                        <Text>Gang</Text>
+                    </View>
+                </Modal>
+            </SafeAreaView>
+        </ClickOutsideProvider>
+    );
+}
+
+const ScheduleButton = () => {
+    return (
+        <TouchableOpacity>
+            <Text>Register</Text>
+        </TouchableOpacity>
     );
 }
 
@@ -116,7 +159,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
-    card: {
+    button: {
 
     },
 });
