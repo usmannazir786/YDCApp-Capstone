256ac87c9f18cb89d7599628436bf03f5f843a99
Carter <cdsarney@gmail.com>
2024-03-28 01:38:39 -0600

    IDEK ANYMORE I GIVE UP

--- src/screens/ChatList.jsx
+++ src/screens/ChatList.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect, StyleSheet} from 'react';
+import React, { useState, useEffect} from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
 
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../Firebase/firebaseConfig'; 
 
-const ChatList = () => {
+function ChatList({navigation}) {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
@@ -21,20 +22,18 @@ const ChatList = () => {
         };
 
         fetchUsers();
-    }, []);
+    }, [navigation]);
 
    
 
     return (
         <FlatList
         data={users}
-      //   keyExtractor={(item)=>item.uid}
         renderItem={({item}) => (
         <TouchableOpacity onPress={() => navigation.navigate('Chat', {name: item.name, uid: item.uid})} >
             <View style={styles.card} >
               <View style={styles.textArea}>
             <Text style={styles.nameText} >{item.name}</Text>
-            <Text style={styles.msgContent} >{item.email}</Text>
             </View>
             </View>
             </TouchableOpacity>
@@ -94,7 +93,6 @@ const styles = StyleSheet.create({
   nameText: {
     fontSize: 14,
     fontWeight: '900',
-    fontFamily: 'Verdana'
   },
   msgTime: {
     textAlign: 'right',
