b501448801a4fb0228576dc262bc7075c504040e
Carter <cdsarney@gmail.com>
2024-04-01 19:44:32 -0600

    TabBar also kinda done

--- App.jsx
+++ App.jsx
@@ -9,6 +9,7 @@ import FoodInput from './src/screens/Food';
 import HomePage from './src/screens/HomeScreen';
 import Chat from './src/screens/Chat';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/screens/Login';
 import Signup from './src/screens/Signup';
@@ -16,7 +17,7 @@ import { PaperProvider } from 'react-native-paper';
 import Polling from './src/screens/components/Polling';
 import ChatList from './src/screens/ChatList';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
 
@@ -25,21 +26,21 @@ const App = () => {
           
         <PaperProvider>
         <NavigationContainer>
-          <Stack.Navigator
+          <Tab.Navigator
             screenOptions={{
               headerShown: false,
             }}
           >
-            <Stack.Screen name="Login Auth" component={Login}/>
-            <Stack.Screen name="Signup Auth" component={Signup}/>
-            <Stack.Screen name="Youth Drop-In Center" component={HomePage}/>
-            <Stack.Screen name="Schedule" component={Schedule}/>
-            <Stack.Screen name="Food" component={FoodInput}/>
-            <Stack.Screen name="Chat" component={Chat}/>
-            <Stack.Screen name="Polling" component={Polling}/>
-            <Stack.Screen name="ChatList" component={ChatList}/>
+            <Tab.Screen name="Login Auth" component={Login}/>
+            <Tab.Screen name="Signup Auth" component={Signup}/>
+            <Tab.Screen name="Youth Drop-In Center" component={HomePage}/>
+            <Tab.Screen name="Schedule" component={Schedule}/>
+            <Tab.Screen name="Food" component={FoodInput}/>
+            <Tab.Screen name="Chat" component={Chat}/>
+            <Tab.Screen name="Polling" component={Polling}/>
+            <Tab.Screen name="ChatList" component={ChatList}/>
 
-          </Stack.Navigator>
+          </Tab.Navigator>
         </NavigationContainer>
       </PaperProvider>
     );
